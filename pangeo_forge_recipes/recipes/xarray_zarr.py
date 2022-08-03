8c40f553e107301ec2bd16970a06268ffe7398d5
Alex Merose <alxr@google.com>
2022-08-03 13:45:59 -0700

    Revert change to previously working version.

--- pangeo_forge_recipes/recipes/xarray_zarr.py
+++ pangeo_forge_recipes/recipes/xarray_zarr.py
@@ -656,7 +656,7 @@ def finalize_target(*, config: XarrayZarrRecipe) -> None:
         # https://github.com/pangeo-forge/pangeo-forge-recipes/issues/214
         # filter out the dims from the array metadata not in the Zarr group
         # to handle coordinateless dimensions.
-        dims = (dim for dim in _gather_coordinate_dimensions(group) if dim in group)
+        dims = set(_gather_coordinate_dimensions(group)) & set(group)
         for dim in dims:
             arr = group[dim]
             attrs = dict(arr.attrs)
